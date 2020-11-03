9162241c888ae91b52e0cb7f349ad9e1e53f2d22
MH <38198319+FlyBrundle@users.noreply.github.com>
2020-11-02 18:18:20 -0600

    Update submit.py
    Added docstring to SubmitForm Function

--- submit.py
+++ submit.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 class SubmitForm(FlaskForm):
+    ''' Create a simple form for sending transactions '''
     amount = StringField('amount')
     submit = SubmitField('Send')
     receiver = StringField('receiver')
